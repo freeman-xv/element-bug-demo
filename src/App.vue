219378777561fdc855279966b45c69c48986ed6e
freeman <freeman_xv@sina.com>
2024-02-27 16:11:09 +0800

    first version

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-button ref="btnRef" type="primary" @click="open = true">
+    Begin Tour
+  </el-button>
+
+  <el-tour v-model="open">
+    <el-tour-step
+      title="Center"
+      description="Displayed in the center of screen."
+    />
+    <el-tour-step
+      title="Right"
+      description="On the right of target."
+      placement="right"
+      :target="btnRef?.$el"
+    />
+    <el-tour-step
+      title="Top"
+      description="On the top of target."
+      placement="top"
+      :target="btnRef?.$el"
+    />
+  </el-tour>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const btnRef = ref()
+const open = ref(false)
+</script>
